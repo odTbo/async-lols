755d506a8a6089c3ff5cb4c0165dfeec93c8c4ea
Vladimir <htcvlado@gmail.com>
2021-09-29 19:32:13 +0200

    lols

--- async_main.py
+++ async_main.py
@@ -9,81 +9,75 @@ import time
 import asyncio
 
 servers = ["EUW", "EUNE"]
+wl_path = "english-words-master/words.txt"
 
 
 def create_wordlist() -> set:
-    # CREATE ENGLISH LANGUAGE WORDLIST SET
-    print("Creating wordlist...")
-    with open("english-words-master/words.txt") as file:
+    """Creates set of clean english words."""
+
+    with open(wl_path) as file:
         wordlist = {word.strip().lower() for word in file.readlines()}
-    print("DONE.\n")
+    print("Created wordlist.")
 
     return wordlist
 
 
 async def get_html(s: ClientSession, link: str, server: str) -> dict:
-    # TODO aiohttp module session
-    # before = time.time()
+    """Fetches raw html from target server url."""
     print("Fetching {}...".format(server))
     link = link.format(server.lower())
-    # print(link)
+
     async with s.get(url=link, headers=headers) as r:
         html = await r.text()
         output = {
             "server": server,
             "html": html
         }
-        # after = time.time()
-        # result = round(after - before, 2)
-        # print("Scraping of {} took: {}s".format(server, result))
 
-        # await parse_html(html.decode(), server)
         print("Fetched {}".format(server))
         return output
 
 
 def parse_html(wordlist: set, content: dict) -> None:
-    # PROCESS THE RESPONSE WITH BEAUTIFUL SOUP
+    """Processes raw html with BeautifulSoup."""
     html = content["html"]
     server = content["server"]
-    print("Parsing {}...".format(server))
+
     soup = Bs(html, "html.parser")
     table = soup.select(css_selector)
-    # print(f"Number of potential names: {len(table)}")
     assert len(table) != 0
 
+    print(f"Number of potential names: {len(table)}")
+
     data = []
-    # print("Fetching names...")
     for row in table:
 
-        # GET EACH SUMMONER NAME AND IT's AVAILABILITY
+        # Get each summoner name and it's availability
         summoner_name = row.select_one("td > a").text.lower()
         availability = row.select("td")[1].select_one("span").text.strip()[0:-1]
         if summoner_name in wordlist:
 
-            # ADD NAME, AVAILABILITY TUPLE TO OUTPUT LIST
+            # Add (name, availability) to output list
             line = (availability, summoner_name)
             data.append(line)
+
     assert len(data) != 0
-    # print(f"Number of {server} names fetched: {len(data)}")
-    # after = time.time()
-    # result = round(after - before, 2)
-    # print("Parsing data for {} took: {}s".format(server, result))
+
     print("Parsed {}".format(server))
 
     save_data(data, server)
 
 
 def save_data(data, server) -> None:
-    # TRANSFORM OUTPUT LIST TO DICTIONARY
+    """Outputs data to json file."""
+    # Transform data to dictionary {'days': [name, name...], ...}
     d = defaultdict(list)
     for k, v in data:
         d[k].append(v)
 
-    # WRITE OUTPUT TO JSON FILE
-    # print("Saving as JSON file...")
     with open(f"{server}.json", "w") as f:
         json.dump(d, f, indent=4)
+    print("Saved {} json file.".format(server))
 
 
 async def main():
